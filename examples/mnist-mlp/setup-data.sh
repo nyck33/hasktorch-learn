042856d564a2e3d6ac1ae3baec8e887ed4888aaf
nyck33 <nobutaka.gold3@gmail.com>
2024-05-05 12:21:52 +0900

    edited mnist data source from issues

--- examples/mnist-mlp/setup-data.sh
+++ examples/mnist-mlp/setup-data.sh
@@ -2,9 +2,13 @@
 
 mkdir data
 pushd data
-wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
-wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
-wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
-wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+#wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+wget https://ossci-datasets.s3.amazonaws.com/mnist/train-images-idx3-ubyte.gz
+#wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+wget https://ossci-datasets.s3.amazonaws.com/mnist/train-labels-idx1-ubyte.gz
+#wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+wget https://ossci-datasets.s3.amazonaws.com/mnist/t10k-images-idx3-ubyte.gz
+#wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+wget https://ossci-datasets.s3.amazonaws.com/mnist/t10k-labels-idx1-ubyte.gz
 popd
 
